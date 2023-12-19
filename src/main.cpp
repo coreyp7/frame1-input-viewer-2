aada16c79348eb3da15d70dcbe535e739af4558e
Corey Pierce <coreypierce11@gmail.com>
2023-12-18 20:10:04 -0500

    Initial commit
    Setup project and sdl init

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <SDL.h>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+int WINDOW_WIDTH = 500;
+int WINDOW_HEIGHT = 500;
+
+int main(int, char**) {
+	printf("hello, world");
+
+	// Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+	{
+		printf("Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	// From 2.0.18: Enable native IME.
+#ifdef SDL_HINT_IME_SHOW_UI
+	SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+	// Create window with SDL_Renderer graphics context
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+	window = SDL_CreateWindow("QuadTree Collision Detection demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, window_flags);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); //SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr)
+	{
+		SDL_Log("Error creating SDL_Renderer!");
+		return 1;
+	}
+
+	bool quit = false;
+	SDL_Event event;
+
+	while (!quit) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderPresent(renderer);
+		SDL_RenderClear(renderer);
+	}
+
+	return 0;
+}
